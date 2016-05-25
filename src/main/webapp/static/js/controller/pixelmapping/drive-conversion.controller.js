a7db32060be0c550b4e37c3fd0a091bb0433f3bc
YI ZHAO <yizhao.cs@gmail.com>
2016-05-24 19:24:08 -0700

    added Yield to Call

--- src/main/webapp/static/js/controller/pixelmapping/drive-conversion.controller.js
+++ src/main/webapp/static/js/controller/pixelmapping/drive-conversion.controller.js
@@ -4,37 +4,14 @@
 
 'use strict';
 
-app.controller('listCtrlDeriveConversion', function ($scope, pixelmappingService) {
-    pixelmappingService.getMappings('derive-conversion').then(function (backendData) {
-        $scope.frontendData = backendData.data;
-    });
-});
+app.controller('listCtrlDeriveConversion', function ($scope) {
+    $scope.multiply = function multiply(){
+        var coupon=$scope.frontendData.coupon;
+        var par=$scope.frontendData.par;
+        var year=$scope.frontendData.year;
+        var price=$scope.frontendData.price;
 
-app.controller('editCtrlDeriveConversion', function ($scope, $rootScope, $location, $routeParams, pixelmappingService, backendData) {
-    var mappingID = ($routeParams.mappingID) ? parseInt($routeParams.mappingID) : 0;
-    $rootScope.title = (mappingID > 0) ? 'Edit Mapping' : 'Add Mapping';
-    $scope.buttonText = (mappingID > 0) ? 'Update Mapping' : 'Add New Mapping';
-    $scope.isUpdate = (mappingID > 0) ? true : false; // false to get rid of "Delete" button
-
-    $scope.frontendData = angular.copy(backendData.data);
-
-    $scope.isClean = function () {
-        return angular.equals(backendData.data, $scope.frontendData);
+        var c=((coupon+(par-price)/year)/((par+price)/2))*100;
+        $scope.frontendData.total=c;
     }
-
-    $scope.deleteMapping = function (frontendData) {
-        // $location.path('/adobe');
-        if (confirm("Are you sure to delete mapping number: " + frontendData.dp_key_id) == true)
-            pixelmappingService.deleteMapping($rootScope.base + 'derive-conversion', frontendData.dp_key_id, 'derive-conversion');
-    };
-
-    $scope.saveMapping = function (frontendData) {
-        // $location.path('/adobe');
-        if (mappingID <= 0) {
-            pixelmappingService.insertMapping($rootScope.base + 'derive-conversion', frontendData, 'derive-conversion');
-        }
-        else {
-            pixelmappingService.updateMapping($rootScope.base + 'derive-conversion', mappingID, frontendData, 'derive-conversion');
-        }
-    };
 });
